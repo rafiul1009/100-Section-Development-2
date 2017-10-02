e1c0618ac70d2c3eaadcdc2f4c9e64b4cc03128f
rafiul1009 <rafiul1009@gmail.com>
2017-10-02 22:33:16 +0600

    class10-gradient-bg-owl-carousel-hero-area

--- class10-gradient-bg-owl-carousel-hero-area/assets/js/main.js
+++ class10-gradient-bg-owl-carousel-hero-area/assets/js/main.js
@@ -3,7 +3,13 @@
 
 
     jQuery(document).ready(function ($) {
-        
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            nav: true,
+            navText: ["<i class='fa fa-long-arrow-left'></i>" , "<i class='fa fa-long-arrow-right'></i>"]
+        });
     });
 
 
