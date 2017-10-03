ddf89e1104d0cabc9ed71a8669e0f534f7f0c83c
rafiul1009 <rafiul1009@gmail.com>
2017-10-03 21:38:38 +0600

    class11-pricing-table

---
+++ class11-pricing-table/assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+    "use strict";
+
+
+    jQuery(document).ready(function ($) {
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            nav: true,
+            navText: ["<i class='fa fa-long-arrow-left'></i>" , "<i class='fa fa-long-arrow-right'></i>"]
+        });
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));	
